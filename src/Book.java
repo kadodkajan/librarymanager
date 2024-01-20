71cec16a31b527aa1f80e1a96bb20503c600d6df
Nava <sarvanava@gmail.com>
2024-01-20 16:22:52 -0500

    first

---
+++ src/Book.java
@@ -0,0 +1,44 @@
+public class Book {
+    // Attributes
+    private int bookID;
+    private String title;
+    private String author;
+    private String genre;
+    private boolean availability;
+
+    // Constructor
+    public Book(int bookID, String title, String author, String genre) {
+        this.bookID = bookID;
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        // new book will available
+        this.availability = true;
+    }
+
+    // Getter methods
+    public int getBookID() {
+        return bookID;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public String getGenre() {
+        return genre;
+    }
+
+
+
+    public boolean isAvailable() {
+        return availability;
+    }
+
+    // Setter method for changing availability status
+    public void setAvailability(boolean availability) {
+        this.availability = availability;
+    }
+
+}
---
+++ src/Library.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+public class Library {
+    private  List<Book> books;
+
+    public Library() {
+        this.books = new ArrayList<>();
+    }
+
+    // Method to add a book to the library
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+
+    // Method to remove a book from the library
+    public void removeBook(Book book) {
+        books.remove(book);
+    }
+
+    // Method to check the availability of a book
+    public boolean isBookAvailable(Book book) {
+        return books.contains(book) && book.isAvailable();
+    }
+
+    public List<Book> getAvailableBooks() {
+        List<Book> availableBooks = new ArrayList<>();
+        for (Book book : books) {
+            if (book.isAvailable()) {
+                availableBooks.add(book);
+            }
+        }
+        return availableBooks;
+    }
+    public List<Book> getALLBooks() {
+
+        return books;
+    }
+}
